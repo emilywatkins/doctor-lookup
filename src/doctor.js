e5eb05960fd5c3700f9e654c75e4001abe768bc6
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-23 11:26:38 -0700

    incorporate search term as query in api call.

--- src/doctor.js
+++ src/doctor.js
@@ -5,9 +5,9 @@ export function getData() {
   return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
     let key = process.env.apiKey;
-    console.log(process.env.apiKey);
-    //0f6e819a206733bb1e906a5564e0548e
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=broken%20arm&location=45.5231%2C-122.6765%2C25&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${key}`;
+    let queryInput = $('#query').val();
+    let query = queryInput.replace(/ /g,"%20");
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=45.5231%2C-122.6765%2C25&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${key}`;
 
     request.onload = function() {
       if (this.status === 200) {
