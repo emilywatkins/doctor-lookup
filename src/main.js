74f1e47863ef5d89818de789f764d2e2e6d83f96
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-23 08:57:47 -0700

    initialize basic file setup.

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Doctor } from './doctor-api.js'
+
+
+$(document).ready(function() {
+  $('#').submit(function(event) {
+    event.preventDefault();
+
+  });
+
+});
