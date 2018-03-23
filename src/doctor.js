af57da591990df52ce85b7c02f3abebdc6f12d5d
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-23 15:11:57 -0700

    add website to output.

--- src/doctor.js
+++ src/doctor.js
@@ -9,7 +9,7 @@ export function getData() {
     let query = queryInput.replace(/ /g,"%20");
     let nameInput = $('#name').val();
     let name = nameInput.replace(/ /g,"%20");
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=45.5231%2C-122.6765%2C25&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${key}`;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location=45.5231%2C-122.6765%2C25&user_location=45.5231%2C-122.6765&skip=0&limit=5&user_key=${key}`;
 
     console.log(url);
     request.onload = function() {
--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { getData } from './doctor.js'
-//${body.data[i].phones.number}
-//${body.data[i].visit_address.street}
-//${body.data[i].visit_address.city}, ${body.data[i].visit_address.city} ${body.data[i].visit_address.zip}
+//= `${body.data[i].practices[j].website}`;
 $(document).ready(function() {
   $('#input').submit(function(event) {
     event.preventDefault();
@@ -20,27 +18,23 @@ $(document).ready(function() {
       for(var i = 0; i < body.data.length; i++) {
         let name = `${body.data[i].profile.first_name} ${body.data[i].profile.last_name}`;
         for(var j = 0; j < body.data[i].practices.length; j++) {
-          let newPatients = `${body.data[i].practices[j].accepts_new_patients}`;
-          let practiceName = `${body.data[i].practices[j].name}`;
-          let practiceStreet = `${body.data[i].practices[j].visit_address.street}`;
-          let practiceCity = `${body.data[i].practices[j].visit_address.city}`;
-          let practiceState = `${body.data[i].practices[j].visit_address.state}`;
-          let practiceZip = `${body.data[i].practices[j].visit_address.zip}`;
-          let practicePhone = `${body.data[i].practices[j].phones[0].number}`;
+          let newPatients = body.data[i].practices[j].accepts_new_patients;
+          let practiceName = body.data[i].practices[j].name;
+          let practiceStreet = body.data[i].practices[j].visit_address.street;
+          let practiceCity = body.data[i].practices[j].visit_address.city;
+          let practiceState = body.data[i].practices[j].visit_address.state;
+          let practiceZip = body.data[i].practices[j].visit_address.zip;
+          let practicePhone = body.data[i].practices[j].phones[0].number;
+          let practiceWebsite = body.data[i].practices[j].website;
+
           $('#doctor-data').append(
-            `<div>${name}
-            <br>
-            Practice: ${practiceName}
-            <br>
-            accepting new patients? ${newPatients}
-            <br>
-            ${practiceStreet}
-            <br>
-            ${practiceCity}, ${practiceState} ${practiceZip}
-            <br>
-            ${practicePhone}
-            <br>
-            <br>
+            `<div class="well">${name}<br>
+            Practice: ${practiceName}<br>
+            Accepting new patients? ${newPatients}<br>
+            ${practiceStreet}<br>
+            ${practiceCity}, ${practiceState} ${practiceZip}<br>
+            ${practicePhone}<br>
+            Website: ${practiceWebsite}<br><br>
             </div>`
           );
         }
