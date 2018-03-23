8cc2e879021575374179d08e7a9ce75a757c3152
Emily Watkins <emily.e.dolliver@gmail.com>
2018-03-23 11:13:24 -0700

    got stuck for an hour on getting process.env to return the api key. fixed, moving forward on inputting queries.

--- src/doctor.js
+++ src/doctor.js
@@ -4,7 +4,11 @@ export function getData() {
 
   return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
-    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=0f6e819a206733bb1e906a5564e0548e';
+    let key = process.env.apiKey;
+    console.log(process.env.apiKey);
+    //0f6e819a206733bb1e906a5564e0548e
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=broken%20arm&location=45.5231%2C-122.6765%2C25&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${key}`;
+
     request.onload = function() {
       if (this.status === 200) {
         resolve(request.response);
